7aa88d8dea27cb9c9d22f8c584abc62eaa78928e
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-02 13:21:54 +0200

    added PowerSource peripheral

---
+++ Peripheral/Src/PowerSource.cpp
@@ -0,0 +1,18 @@
+/*
+ * PowerSource.cpp
+ *
+ *  Created on: Aug 2, 2019
+ *      Author: timm
+ */
+
+#include "PowerSource.h"
+
+void PowerSource::initialize()
+{
+
+}
+
+void PowerSource::shutdown()
+{
+
+}
