436a97880a22e3d559fef8623e5cf91bf7701ab9
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-06 22:22:39 +0200

    removed debugging function in main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -44,15 +44,15 @@ int main(void)
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
-  /** Enable the PVD Output  */
-  LL_PWR_EnablePVD();
-
   /* Configure the system clock */
   SystemClock_Config();
 
+  /** Enable the PVD Output  */
+  //LL_PWR_EnablePVD();
+
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_TIM1_Init();
+  //MX_GPIO_Init();
+  //MX_TIM1_Init();
 
 #ifdef DEBUG
   LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_DBGMCU);
@@ -61,22 +61,13 @@ int main(void)
   LL_APB1_GRP1_DisableClock(LL_APB1_GRP1_PERIPH_DBGMCU);
 #endif
 
-  PWR->PDCRA = PWR_PDCRA_PD8;
-  PWR->CR3 |= PWR_CR3_APC | PWR_CR3_ENB_ULP;
-  LL_GPIO_SetOutputPin(POWER_MODE_GPIO_Port, POWER_MODE_Pin);
-
-  LL_SYSTICK_EnableIT();
   LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
   LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
 
-  LL_RTC_DisableWriteProtection(RTC);
-  LL_RTC_BKP_SetRegister(TAMP, 0, 0x66);
-  LL_RTC_EnableWriteProtection(RTC);
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
     manager.runTasks();
-    manager.getSystemTickReference().init();
   }
 }
