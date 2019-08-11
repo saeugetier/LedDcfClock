460399b50ae45734bddbfb482fe16b49a64c4d47
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-11 13:52:10 +0200

    added new SettingsTask

---
+++ Inc/SettingsTask.h
@@ -0,0 +1,15 @@
+/*
+ * SettingsTask.h
+ *
+ *  Created on: 08.08.2019
+ *      Author: timm
+ */
+
+#ifndef INC_SETTINGSTASK_H_
+#define INC_SETTINGSTASK_H_
+
+
+
+
+
+#endif /* INC_SETTINGSTASK_H_ */
