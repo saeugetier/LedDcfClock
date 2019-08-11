d1b117323658c3369c2f883f2e0d72a5667bb0d0
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-11 17:42:33 +0200

    implementing functions of buttons

--- Peripheral/Src/Buttons.cpp
+++ Peripheral/Src/Buttons.cpp
@@ -10,66 +10,82 @@
 #include "stm32g0xx_ll_gpio.h"
 #include "main.h"
 
-Button::Button(uint32_t pin)
+Button::Button(ButtonPin pin)
 {
 	mPin = pin;
+
+	switch(pin)
+	{
+	case PUSH_BUTTON_PIN:
+		LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN1);
+		LL_PWR_SetWakeUpPinPolarityLow(LL_PWR_WAKEUP_PIN1);
+		break;
+	case SETTINGS1_BUTTON_PIN:
+		LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN2);
+		LL_PWR_SetWakeUpPinPolarityLow(LL_PWR_WAKEUP_PIN2);
+		break;
+	case SETTINGS2_BUTTON_PIN:
+		LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN4);
+		LL_PWR_SetWakeUpPinPolarityLow(LL_PWR_WAKEUP_PIN4);
+		break;
+	}
 }
 
 void Button::initialize()
 {
-	setPinsToInterrupt();
+	setPinToInterrupt();
 }
 
 void Button::shutdown()
 {
-	setPinsToWakeup();
+	disableInterrupt();
 }
 
-void Button::setPinsToWakeup()
+void Button::disableInterrupt()
 {
-	LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN1);
-	LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN2);
-	LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN4);
+
 }
 
-void Button::setPinsToInterrupt()
+void Button::setPinToInterrupt()
 {
 	LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
-
-	/* Disable Wakeup before using the interrupts */
-	LL_PWR_DisableWakeUpPin(LL_PWR_WAKEUP_PIN1);
-	LL_PWR_DisableWakeUpPin(LL_PWR_WAKEUP_PIN2);
-	LL_PWR_DisableWakeUpPin(LL_PWR_WAKEUP_PIN4);
 
 	/* GPIO Port Clock Enable */
 	LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOA);
 
-	LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE0);
-	EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
-	EXTI_InitStruct.LineCommand = ENABLE;
-	EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
-	EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
-	LL_EXTI_Init(&EXTI_InitStruct);
-	LL_GPIO_SetPinPull(PUSHBUTTON_GPIO_Port, PUSHBUTTON_Pin, LL_GPIO_PULL_NO);
-	LL_GPIO_SetPinMode(PUSHBUTTON_GPIO_Port, PUSHBUTTON_Pin, LL_GPIO_MODE_INPUT);
-
-	LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE4);
-	EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_4;
-	EXTI_InitStruct.LineCommand = ENABLE;
-	EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
-	EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
-	LL_EXTI_Init(&EXTI_InitStruct);
-	LL_GPIO_SetPinPull(SETTINGS1_GPIO_Port, SETTINGS1_Pin, LL_GPIO_PULL_NO);
-	LL_GPIO_SetPinMode(SETTINGS1_GPIO_Port, SETTINGS1_Pin, LL_GPIO_MODE_INPUT);
-
-	LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE2);
-	EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_2;
-	EXTI_InitStruct.LineCommand = ENABLE;
-	EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
-	EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
-	LL_EXTI_Init(&EXTI_InitStruct);
-	LL_GPIO_SetPinPull(SETTINGS2_GPIO_Port, SETTINGS2_Pin, LL_GPIO_PULL_NO);
-	LL_GPIO_SetPinMode(SETTINGS2_GPIO_Port, SETTINGS2_Pin, LL_GPIO_MODE_INPUT);
+	switch(mPin)
+	{
+	case PUSH_BUTTON_PIN:
+		LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE0);
+		EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_0;
+		EXTI_InitStruct.LineCommand = ENABLE;
+		EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+		EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
+		LL_EXTI_Init(&EXTI_InitStruct);
+		LL_GPIO_SetPinPull(PUSHBUTTON_GPIO_Port, PUSHBUTTON_Pin, LL_GPIO_PULL_NO);
+		LL_GPIO_SetPinMode(PUSHBUTTON_GPIO_Port, PUSHBUTTON_Pin, LL_GPIO_MODE_INPUT);
+		break;
+	case SETTINGS1_BUTTON_PIN:
+		LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE4);
+		EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_4;
+		EXTI_InitStruct.LineCommand = ENABLE;
+		EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+		EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
+		LL_EXTI_Init(&EXTI_InitStruct);
+		LL_GPIO_SetPinPull(SETTINGS1_GPIO_Port, SETTINGS1_Pin, LL_GPIO_PULL_NO);
+		LL_GPIO_SetPinMode(SETTINGS1_GPIO_Port, SETTINGS1_Pin, LL_GPIO_MODE_INPUT);
+		break;
+	case SETTINGS2_BUTTON_PIN:
+		LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTA, LL_EXTI_CONFIG_LINE2);
+		EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_2;
+		EXTI_InitStruct.LineCommand = ENABLE;
+		EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+		EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING;
+		LL_EXTI_Init(&EXTI_InitStruct);
+		LL_GPIO_SetPinPull(SETTINGS2_GPIO_Port, SETTINGS2_Pin, LL_GPIO_PULL_NO);
+		LL_GPIO_SetPinMode(SETTINGS2_GPIO_Port, SETTINGS2_Pin, LL_GPIO_MODE_INPUT);
+		break;
+	}
 }
 
 bool Button::getButtonPressed()
@@ -79,16 +95,39 @@ bool Button::getButtonPressed()
 
 bool Button::isWakeupFlagSet()
 {
-	if(LL_PWR_IsActiveFlag_WU1() || LL_PWR_IsActiveFlag_WU2() || LL_PWR_IsActiveFlag_WU4())
-		return true;
-	return false;
+	bool result = false;
+	switch(mPin)
+	{
+	case PUSH_BUTTON_PIN:
+		result = LL_PWR_IsActiveFlag_WU1();
+		break;
+	case SETTINGS1_BUTTON_PIN:
+		result = LL_PWR_IsActiveFlag_WU2();
+		break;
+	case SETTINGS2_BUTTON_PIN:
+		result = LL_PWR_IsActiveFlag_WU4();
+		break;
+	default:
+		break;
+	}
+
+	return result;
 }
 
-void Button::clearWakeupFlags()
+void Button::clearWakeupFlag()
 {
-	LL_PWR_ClearFlag_WU1();
-	LL_PWR_ClearFlag_WU2();
-	LL_PWR_ClearFlag_WU4();
+	switch(mPin)
+	{
+	case PUSH_BUTTON_PIN:
+		LL_PWR_ClearFlag_WU1();
+		break;
+	case SETTINGS1_BUTTON_PIN:
+		LL_PWR_ClearFlag_WU2();
+		break;
+	case SETTINGS2_BUTTON_PIN:
+		LL_PWR_ClearFlag_WU4();
+		break;
+	}
 }
 
 void Button::handleInterrupt()
