fdbaf9203634314af9f7a99c2d5d170fd4726506
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-11 13:52:35 +0200

    added PowerSupervisorTask

---
+++ Inc/PowerSupervisorTask.h
@@ -0,0 +1,15 @@
+/*
+ * PowerSupervisorTask.h
+ *
+ *  Created on: 08.08.2019
+ *      Author: timm
+ */
+
+#ifndef INC_POWERSUPERVISORTASK_H_
+#define INC_POWERSUPERVISORTASK_H_
+
+
+
+
+
+#endif /* INC_POWERSUPERVISORTASK_H_ */
