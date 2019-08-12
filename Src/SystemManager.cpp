bfd68db5ec28a77740d51266786adc898bf7b4d9
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-12 21:21:37 +0200

    corrected order of initialization

--- Src/SystemManager.cpp
+++ Src/SystemManager.cpp
@@ -8,12 +8,15 @@
 #include "SystemManager.h"
 
 SystemManager::SystemManager() :
-		mPulseDetector(false), mDcfPulseCallback(mTaskManager), mPowerSourceCallback(
-				mTaskManager), mDcfPowerDownCallback(mTaskManager), mDcfWakeUpCallback(
-				mTaskManager), mSysTickCallback(mTaskManager), mPowerSupplyCallback(
-				mTaskManager), mDcf77DecodeTask(mPulseDetector, mClock,
-				mDcfWakeup, mDcfPowerdown), mLedClockTask(mClock, mWS2812,
-				mPushButton, mSystemTick)
+		mPulseDetector(false),
+		mDcfPulseCallback(mTaskManager),
+		mPowerSourceCallback(mTaskManager),
+		mDcfPowerDownCallback(mTaskManager),
+		mDcfWakeUpCallback(mTaskManager),
+		mSysTickCallback(mTaskManager),
+		mPowerSupplyCallback(mTaskManager),
+		mLedClockTask(mClock, mWS2812, mPushButton, mSystemTick),
+		mDcf77DecodeTask(mPulseDetector, mClock, mDcfWakeup, mDcfPowerdown)
 {
 	mPulseDetector.getInstance().registerCallback(&mDcfPulseCallback);
 	mDcfWakeup.getInstance().registerCallback(&mDcfWakeUpCallback);
