230f76f7e2a0d9a95a5540776b011af5227d4089
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-14 10:51:28 +0200

    implementation of PowerSource

--- Peripheral/Src/PowerSource.cpp
+++ Peripheral/Src/PowerSource.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "PowerSource.h"
+#include "main.h"
 
 template<>
 PowerSource* InterruptPeripheral<PowerSource>::mPeripheralInstance = nullptr;
@@ -17,10 +18,28 @@ PowerSource::PowerSource()
 
 void PowerSource::initialize()
 {
+	LL_IOP_GRP1_EnableClock(LL_IOP_GRP1_PERIPH_GPIOB);
+	LL_EXTI_InitTypeDef EXTI_InitStruct = { 0 };
+	LL_EXTI_SetEXTISource(LL_EXTI_CONFIG_PORTB, LL_EXTI_CONFIG_LINE1);
+	EXTI_InitStruct.Line_0_31 = LL_EXTI_LINE_1;
+	EXTI_InitStruct.LineCommand = ENABLE;
+	EXTI_InitStruct.Mode = LL_EXTI_MODE_IT;
+	EXTI_InitStruct.Trigger = LL_EXTI_TRIGGER_RISING_FALLING;
+	LL_EXTI_Init(&EXTI_InitStruct);
+	LL_GPIO_SetPinPull(POWER_SOURCE_GPIO_Port, POWER_SOURCE_Pin, LL_GPIO_PULL_NO);
+	LL_GPIO_SetPinMode(POWER_SOURCE_GPIO_Port, POWER_SOURCE_Pin, LL_GPIO_MODE_INPUT);
 
+	NVIC_EnableIRQ (EXTI0_1_IRQn);
+	NVIC_SetPriority(EXTI0_1_IRQn, 0);
 }
 
 void PowerSource::shutdown()
 {
+	LL_GPIO_SetPinMode(POWER_SOURCE_GPIO_Port, POWER_SOURCE_Pin, LL_GPIO_MODE_ANALOG);
+}
 
+void PowerSource::handleInterrupt()
+{
+	if(mCallback != nullptr)
+		mCallback->notify();
 }
