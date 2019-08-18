4b57ca86b0a8405ef7602a36dba1765fbccbd3e0
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-18 16:29:36 +0200

    Settings doesn't access hardware directly. Uses SettingsStorage now.

--- Inc/Settings.h
+++ Inc/Settings.h
@@ -9,16 +9,21 @@
 #define INC_SETTINGS_H_
 
 #include "main.h"
+#include "PeripheralReference.h"
+#include "SettingsStorage.h"
 
 class Settings
 {
 public:
+	Settings(PeripheralReference<SettingsStorage> storage);
 	uint8_t getBrightness();
 	uint8_t getMode();
 	uint32_t getLastCalibrationTimestamp();
 	void setBrightness(uint8_t brightness);
 	void setMode(uint8_t mode);
 	void setLastCalibrationTimestamp(uint32_t timeStamp);
+protected:
+	PeripheralReference<SettingsStorage> mStorage;
 };
 
 
--- Src/Settings.cpp
+++ Src/Settings.cpp
@@ -9,39 +9,38 @@
 
 /* Backup registers are use, because they are not reset in standby mode */
 
+Settings::Settings(PeripheralReference<SettingsStorage> storage) : mStorage(storage)
+{
+	mStorage.init();
+}
+
 uint8_t Settings::getBrightness()
 {
-	return LL_RTC_BKP_GetRegister(TAMP, LL_RTC_BKP_DR0) & 0xFF;
+	return mStorage.getInstance().readAddress(0) & 0xFF;
 }
 
 uint8_t Settings::getMode()
 {
-	return LL_RTC_BKP_GetRegister(TAMP, LL_RTC_BKP_DR1) & 0xFF;
+	return mStorage.getInstance().readAddress(1) & 0xFF;
 }
 
 void Settings::setBrightness(uint8_t brightness)
 {
-	LL_RTC_DisableWriteProtection(RTC);
-	LL_RTC_BKP_SetRegister(TAMP, LL_RTC_BKP_DR0, brightness);
-	LL_RTC_EnableWriteProtection(RTC);
+	mStorage.getInstance().writeAddress(0, brightness);
 }
 
 void Settings::setMode(uint8_t mode)
 {
-	LL_RTC_DisableWriteProtection(RTC);
-	LL_RTC_BKP_SetRegister(TAMP, LL_RTC_BKP_DR1, mode);
-	LL_RTC_EnableWriteProtection(RTC);
+	mStorage.getInstance().writeAddress(1, mode);
 }
 
 uint32_t Settings::getLastCalibrationTimestamp()
 {
-	return LL_RTC_BKP_GetRegister(TAMP, LL_RTC_BKP_DR3);
+	return mStorage.getInstance().readAddress(3);
 }
 
 void Settings::setLastCalibrationTimestamp(uint32_t timeStamp)
 {
-	LL_RTC_DisableWriteProtection(RTC);
-	LL_RTC_BKP_SetRegister(TAMP, LL_RTC_BKP_DR3, timeStamp);
-	LL_RTC_EnableWriteProtection(RTC);
+	mStorage.getInstance().writeAddress(3, timeStamp);
 }
 
