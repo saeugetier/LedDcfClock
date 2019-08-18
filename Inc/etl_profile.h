f4fe2041f37f6440100578fcc8db0c75f8ffc93a
Timm Eversmeyer <saeugetier@googlemail.com>
2019-08-18 11:53:09 +0200

    added etl profile

---
+++ Inc/etl_profile.h
@@ -0,0 +1,15 @@
+/*
+ * etl_profile.h
+ *
+ *  Created on: Aug 14, 2019
+ *      Author: timm
+ */
+
+#ifndef INC_ETL_PROFILE_H_
+#define INC_ETL_PROFILE_H_
+
+
+#include "etl/profiles/armv6.h"
+
+
+#endif /* INC_ETL_PROFILE_H_ */
